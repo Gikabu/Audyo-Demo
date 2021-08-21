5691166a46d554a09d3c0ac053da99485bd55833
Jonathan Gikabu <jonathangikabu@e-kraal.com>
2021-08-21 13:46:04 +0300

    Initial Commit

---
+++ Audyo/AudyoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudyoApp.swift
+//  Audyo
+//
+//  Created by Jonathan Gikabu on 21/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct AudyoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
